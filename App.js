d4da7d14c10bed4460be5f3a253da5f1c61f8273
Erick Romero <egromero@uc.cl>
2024-01-27 13:03:59 -0700

    view: add buttons and code input

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Button from './components/button.js'
+import CodeInput from './components/codeInput.js'
 
 export default function App() {
+  const [value, setValue] = useState(' ')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+    <View style={styles.buttonContainer}>
+      <CodeInput value={value} setValue={setValue}/>
+      <Button text="Ingresar" icon="enter" onPress={()=>{alert(`${value}`)}}/>
+      <Button text="Crear Sala" icon="create-outline" onPress={()=>{alert("hola grupo")}}/>
+    </View>
     </View>
   );
 }
@@ -13,7 +21,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#25292e',
     alignItems: 'center',
     justifyContent: 'center',
   },

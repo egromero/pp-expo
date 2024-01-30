9e0b8b364d7bd03cf0d37e52c4fd79f25345cc0d
Erick Romero <egromero@uc.cl>
2024-01-30 08:19:14 -0700

    feat: add styles and components to form

--- App.js
+++ App.js
@@ -1,8 +1,5 @@
-import { useState } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Landing from './screens/landing.js';
 import CreateRoom from './screens/createRoom.js';
 
@@ -10,14 +7,15 @@ const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Landing"
-          component={Landing}
-        />
-        <Stack.Screen name="CreateRoom" component={CreateRoom} />
-      </Stack.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Landing"
+            options={{ headerShown: false }}
+            component={Landing}
+            />
+          <Stack.Screen name="CreateRoom" options={{ headerShown: false }} component={CreateRoom} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
--- screens/landing.js
+++ screens/landing.js
@@ -1,26 +1,46 @@
 import { useState } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { Keyboard, View, StyleSheet, TouchableWithoutFeedback } from 'react-native';
 import CodeInput from '../components/codeInput.js'
 import Button from '../components/button.js'
 
 
 export default function Landing({ navigation }) {
-    const [value, setValue] = useState(' ')
+    const [code, setCode] = useState(' ')
+
+    const searchRoom = () => {
+        fetch('https://cc0e-107-190-74-83.ngrok-free.app/room/create', {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({code}),
+            })
+            .then((response) => response.json())
+            .then((json) => {
+                console.log(json)
+            })
+            .catch((error) => {
+                console.error('errores', error);
+            });
+    }
     return (
-    <View style={styles.container}>
-        <View style={styles.buttonContainer}>
-            <CodeInput value={value} setValue={setValue}/>
-            <Button text="Ingresar" icon="enter" onPress={()=>{alert(`${value}`)}}/>
-            <Button text="Crear Sala" icon="create-outline" onPress={() => navigation.navigate('CreateRoom') }/>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <View style={styles.container}>
+            <View style={styles.buttonContainer}>
+                <CodeInput value={code} setValue={setCode}/>
+                <Button text="Ingresar" icon="enter" onPress={searchRoom}/>
+                <Button text="Crear Sala" icon="create-outline" onPress={() => navigation.navigate('CreateRoom') }/>
+            </View>
         </View>
-    </View>
+    </TouchableWithoutFeedback>  
     );
 }
 
 const styles = StyleSheet.create({
     container: {
       flex: 1,
-      backgroundColor: '#25292e',
+      backgroundColor: '#2f4daa',
       alignItems: 'center',
       justifyContent: 'center',
     },
